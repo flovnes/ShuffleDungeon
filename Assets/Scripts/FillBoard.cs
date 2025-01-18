909b902f7831df3e14b4873145cd3a5aabfa0992
flovnes <whyflovnes@gmail.com>
2025-01-18 17:03:26 +0200

    setup the project, add tile prefab

---
+++ Assets/Scripts/FillBoard.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FillBoard : MonoBehaviour {
+    // called once before the first execution of Update() after MonoBehaviour is created
+    void Start() {
+        GenerateBoard();
+    }
+
+    // called once per frame
+    void Update() {
+        
+    }
+}
+
+void GenerateBoard() {
+    // ^
+}
